34b1f70eb9d05eaaa411f705713041e24a53b4b0
duddk <duddk@scatterx.io>
2025-04-18 16:06:45 +0900

    jsx 문법 정리(심화)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import './App.css';
 
 function App() {
   const name = '먹을꺼';
+  const list = ['우유', '딸기', '바나나', '요거트'];
+
   return (
     <>
-      <h1 className='orange'>Hello!</h1>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
       <h2>Hello@</h2>
       <p>{name}</p>
       <ul>
-        <li>우유</li>
-        <li>딸기</li>
-        <li>바나나</li>
+        {
+          list.map((item) => (
+            <li>{item}</li>
+          ))
+        }
       </ul>
-      <img style={{width: '260px', height: '300px'}} src='https://images.unsplash.com/photo-1744063893861-3e703c13d1e4?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <img 
+        style={{width: '260px', height: '300px'}} 
+        src='https://images.unsplash.com/photo-1744063893861-3e703c13d1e4?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        alt='nature' 
+      />
     </>
   );
 }
