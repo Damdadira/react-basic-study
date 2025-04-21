2dd8c8da9acaff9a85334ee5b7ca68c78d832125
duddk <duddk@scatterx.io>
2025-04-21 11:05:20 +0900

    이전 사태 유지하며 count 증가

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,7 +6,16 @@ export default function Counter() {
   return(
     <div className='counter'>
       <span className='number'>{count}</span>
+      
+      {/* 하나씩 증가 */}
       <button className='button' onClick={() => setCount(count + 1)}>Add +</button>
+      
+      {/* 이전 상태 유지 */}
+      {/* <button className='button' onClick={() => {
+        setCount((prev) => prev + 1)
+        setCount((prev) => prev + 1)
+        }}>Add +
+      </button> */}
     </div>
   )
 }
