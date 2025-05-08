7a4cc88a2cdac12f9354c434d28e9f661d92d58a
duddk <duddk@scatterx.io>
2025-05-08 15:28:09 +0900

    invalidateQueries 사용해보기

--- src/components/MainProducts.jsx
+++ src/components/MainProducts.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Products from './Products'
+import { useQueryClient } from '@tanstack/react-query';
 
 export default function MainProducts() {
   const [showLeftProducts, setShowLeftProducts] = useState(true);
   const [showRightProducts, setShowRightProducts] = useState(true);
+  const client = useQueryClient();
 
   return (
     <main style={{display: 'flex', gap: '3rem'}}>
@@ -19,6 +21,11 @@ export default function MainProducts() {
           Toggle
         </button>
       </div>
+      <button onClick={() => {
+        client.invalidateQueries(['products', false])}}
+      >
+        정보가 업데이트 되었음!
+      </button>
     </main>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,7 +13,7 @@ export default function Products(){
   } = useQuery({
     queryKey: ["products", checked],
     queryFn: async () => {
-      console.log("fetching...");
+      console.log("fetching...", checked);
       return fetch(`data/${checked ? "sale_" : ""}products.json`).then((res) => res.json()
       );
     },
